813fec3f2f9d94f2cf7efb8e943c8adb21744fe4
Danielle <dmiz@brandeis.edu>
2019-09-08 12:25:02 +0100

    changes

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,6 +4,18 @@ class Api::V1::CategoriesController < ApplicationController
         render json: Category.all, each_serializer: CategorySerializer
     end 
 
+    def show
+        category=Category.find params[:id]
+        render json: category, include: '*.*.*' 
+    end 
+
+    def update
+        @category=Category.find params[:id]
+        if @category.update(name: params[:name])
+            render json: @category
+        end
+    end
+
     def create
         @category = Category.create(category_params)
         render json: @category, each_serializer: CategorySerializer
